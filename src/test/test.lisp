617804fe2d999fdaa384176c74997a206fcda18e
chaploud <in-the-cloud.950@hotmail.co.jp>
2023-11-30 22:47:46 +0900

    modify syntax highlight

--- src/test/test.lisp
+++ src/test/test.lisp
@@ -1,6 +1,28 @@
 ; Comment
-;; Comment
-;;; Comment
+; TODO:
+; FIXME:
+; XXX:
+; BUG:
+; HACK:
+; NOTE:
+
+'()
+`()
+[]
+{}
+#{}
+""
+"\n"
+#"[0-9]+"
+-999
+0x1f
+0o17
+0b0101
+-3.14e15
+-4/3
+1+2j
+1.-2j
+-2.0e05-1.0e-04j
 
 false
 true
@@ -8,5 +30,4 @@ nil
 if
 when
 cond
-(import hoge as fuga)
 
